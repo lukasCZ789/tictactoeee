083fcb56dcbc18b017f5240fc9b7c2ee2282477c
lukasCZ789 <lukascihelna@gmail.com>
2025-04-02 18:51:43 +0200

    board

--- tictactoeee/Program.cs
+++ tictactoeee/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace tictactoeee
+﻿using System;
+
+namespace tictactoeee
 {
     internal class Program
     {
+        static char[,] board = {
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' }
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Vítej ve hře piškvorky!!");
+            Console.WriteLine();
+            PrintBoard(); // Vykreslí hrací pole
+        }
+
+        static void PrintBoard()
+        {
+            Console.WriteLine("  0 1 2"); // Číslování sloupců
+            Console.Write("0 "); // Číslo řádku
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(board[ 0,i]);
+                if ( i == 0) Console.Write("|");
+            }
+
+            Console.WriteLine(); // Přechod na nový řádek
         }
     }
 }
--- tictactoeee/obj/Debug/net8.0/tictactoeee.AssemblyInfo.cs
+++ tictactoeee/obj/Debug/net8.0/tictactoeee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoeee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02979607cf6e493535b3f6ba49ed9ec105f012b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoeee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoeee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
